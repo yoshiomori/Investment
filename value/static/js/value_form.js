4c7fecd3c29d26acd65451754b75e64357e7c333
yoshio <yoshio.mori@hotmail.com>
2020-02-11 04:43:48 -0300

    Corrigindo local do arquivo js do app value

--- value/static/js/value_form.js
+++ value/static/js/value_form.js
@@ -3,7 +3,7 @@ id_date.pattern = "[0-9]{2}/[0-9]{2}/[0-9]{4}$";
 id_date.placeholder = '##/##/####';
 id_date.oninput = function() {
      this.value = this.value.replace(/(\d{2})(\d{2})(\d{4})/, '$1/$2/$3');
- };
+};
 
  var id_price = document.querySelector('#id_price');
  id_price.placeholder = '###.##'
