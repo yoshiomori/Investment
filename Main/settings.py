acb461c0debe4fcf3b1b5f1b7ed549900dc0bbe4
yoshio <yoshio.mori@hotmail.com>
2020-03-04 21:59:25 -0300

    Todos os hosts são permitidos

--- Main/settings.py
+++ Main/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = eval(os.environ['DEBUG'])
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
