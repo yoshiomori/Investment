f825dd368bebe8c4efebc80aacf89aeada20e78d
yoshio <yoshio.mori@hotmail.com>
2020-05-01 12:52:45 -0300

    Correção das estrutura de arquivos

--- Main/urls.py
+++ Main/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
-from Main import views
+from accounts import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='home'),
