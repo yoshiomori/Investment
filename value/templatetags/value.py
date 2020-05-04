f3acb5eb16a668f23068beddb2bcac79bd0de0d1
yoshio <yoshio.mori@hotmail.com>
2020-05-04 20:31:32 -0300

    Correção do gráfico da variação e do gráfico acumulado

--- value/templatetags/value.py
+++ value/templatetags/value.py
@@ -6,4 +6,9 @@ register = template.Library()
 
 @register.filter
 def sum_group_by_date_value_filter(value_list):
-    return value_list.values('date').annotate(delta=Sum('delta'), price=Sum('price'))
+    d = {}
+    for date, delta, price in value_list.values_list('date', 'delta', 'price'):
+        d.setdefault(date, {'date': date, 'delta': 0, 'price': 0})
+        d[date]['delta'] += delta
+        d[date]['price'] += price
+    return d.values()
